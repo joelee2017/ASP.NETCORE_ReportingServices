16d697a4cadad2db591c5c4915911215c8184fbf
joelee2017 <33776374+joelee2017@users.noreply.github.com>
2022-10-24 12:03:53 +0800

    Update ReportExample.cs

--- AspNetCoreSSRS/ReportExample.cs
+++ AspNetCoreSSRS/ReportExample.cs
@@ -1,8 +1,8 @@
-using KGI.MailComponent.Interface;
-using KGI.MicrosoftTeamsComponent.Interface;
-using KGI.ReportComponent.Helper;
-using KGI.ReportComponent.Interface;
-using KGI.ReportComponent.Model;
+using MailComponent.Interface;
+using MicrosoftTeamsComponent.Interface;
+using ReportComponent.Helper;
+using ReportComponent.Interface;
+using ReportComponent.Model;
 using Microsoft.Extensions.Configuration;
 using ReportExecution2005;
 using SimpleImpersonation;
@@ -115,15 +115,7 @@ namespace KGI.ReportComponent
         /// <returns>byte[]</returns>
         private async Task<byte[]> GetReport2005(ReportModel model)
         {
-            string reportUrl = model.ReportServerWsdlUrl ?? _configuration.GetSection("ReportServerWsdlUrl").Get<string>();
-
-            _reportManager = new ReportManager(reportUrl, _userName, _passWrod, _domain);
-
-            var parameters = _reportManager.ConvertParameterValues(model.Parameters).Result;
-
-            byte[] res = await GetReportByteArray2005(parameters, model.Rerpot_Path, model.Render_Format);
-
-            return res;
+            return await CallReportLoop((x) => GetReportByteArray2005(x), model);
         }
 
         /// <summary>
@@ -132,11 +124,10 @@ namespace KGI.ReportComponent
         /// <param name="model">報表參數</param>
         /// <returns>byte[]</returns>
         private async Task<byte[]> GetReport2010(ReportModel model) 
-            => await TestCallReportLoop((model) 
-                => GetReportByteArray2010(model), model, 0);
+               => await CallReportLoop((x) => GetReportByteArray2010(x), model);
 
         // 透過委派方式整合呼叫失敗時遞迴重新呼叫行為
-        private async Task<byte[]> TestCallReportLoop(Func<ReportModel, Task<byte[]>> func, ReportModel model, int count = 0)
+        private async Task<byte[]> CallReportLoop(Func<ReportModel, Task<byte[]>> func, ReportModel model, int count = 0)
         {
             Task<byte[]> res = default;
             try
@@ -153,15 +144,9 @@ namespace KGI.ReportComponent
                     count++;
 
                     // Timeout 後確認是否有取得資料
-                    byte[] r =  await res;
-                    if (r == null || r.Length == 0)
+                    if (res == null)
                     {
-                        res = this.TestCallReportLoop(func,model,count);
-                    }
-
-                    if (r != null)
-                    {
-                        return await res;
+                        res = this.CallReportLoop(func, model, count);
                     }
                 }
 
@@ -172,36 +157,18 @@ namespace KGI.ReportComponent
         }
 
         // 呼叫報表 2005
-        private async Task<byte[]> GetReportByteArray2005(ParameterValue[] parameters, string path, string renderFormat, int count = 0)
+         private Task<byte[]> GetReportByteArray2005(ReportModel model)
         {
-            byte[] res = default;
-            try
-            {
-                res = await _reportManager.RenderReport(path, renderFormat, parameters);
-            }
-            catch (TimeoutException) // SSRS 呼叫有可能會失敗，失敗後在呼叫最多三次。
-            {
-                //  距離上次呼叫間隔 1 分鐘
-                await Task.Delay(60000);
+            string reportUrl = model.ReportServerWsdlUrl ?? _configuration.GetSection("ReportServerWsdlUrl").Get<string>();
 
-                if (count < 3)
-                {
-                    count++;
-                    if (res == null || res.Length == 0)
-                    {
-                        res = await this.GetReportByteArray2005(parameters, path, renderFormat, count);
-                    }
+            _reportManager = new ReportManager(reportUrl, _userName, _passWrod, _domain);
 
-                    if (res != null)
-                    {
-                        return res;
-                    }
-                }
+            var parameters = _reportManager.ConvertParameterValues(model.Parameters);
 
-                throw;
-            }
+            byte[] bytes = default;
+            bytes = _reportManager.RenderReport(model.Rerpot_Path, model.Render_Format, parameters).Result;
 
-            return res;
+            return Task.FromResult(bytes);
         }
 
         // 呼叫報表 2010
@@ -228,19 +195,6 @@ namespace KGI.ReportComponent
                     List<ParameterModel> newparameter = new List<ParameterModel>();
                     foreach (var par in model.Parameters)
                     {
-                        // {"Name":"WFUND_ID","Value":"L010,K002,H003"} 單一key，帶多參時使用
-                        //var aa = par.Value.ToString().Split(",");
-                        //if (par.Value.ToString().Split(",").Length > 0)
-                        //{
-                        //    foreach (var par2 in par.Value.ToString().Split(","))
-                        //    {
-                        //        report.Params.Add(par.Name, par2);
-                        //    }
-                        //}
-                        //else
-                        //{
-                        //    report.Params.Add(par.Name, par.Value.ToString());
-                        //}
                         report.Params.Add(par.Name, par.Value.ToString());
                     }
                 }
